eea03694c925a4909fc21e99dc33bfc5c008c382
Aliaksei Tcheltsou <aliaksei_tcheltsou@epam.com>
2020-12-20 13:31:22 +0300

    overided two methods in StHand

--- src/main/scala/net/cheltsov/poker/string/StCards.scala
+++ src/main/scala/net/cheltsov/poker/string/StCards.scala
@@ -6,24 +6,33 @@ import net.cheltsov.poker.{Cards, Suit}
 case class StCards(cards: String) extends Cards {
 
   override def toHand: Option[StHand] = if (size == 5) Some(StHand(this)) else None
-  override def +(that: Cards): Cards = StCards(this.cards + that)
-  override def -(that: Cards): Cards = StCards(
-    that.toString.grouped(2).foldLeft(this.cards)(_.replaceAll(_, ""))
+
+  override def +(that: Cards): StCards = StCards(this.cards + that)
+
+  override def -(that: Cards): StCards = StCards(
+    StCards(that).asList.foldLeft(this.cards)(_.replaceAll(_, ""))
   )
 
   override def combineCards(n: Int): List[StCards] =
     asList.combinations(n).map(_.reduce(_ + _)).map(StCards(_)).toList
 
-  override val asPairs: Set[(Rank, Suit)] =
+  override lazy val asPairs: Set[(Rank, Suit)] =
     asList.map(card => (Cards.toRank(card.head.toString), Suit.apply(card.last.toString))).toSet
+
+  override lazy val size: Int = cards.length / 2
 
   override def toString: String = cards
 
-  private def asList: List[String] = cards.grouped(2).toList
+  protected lazy val asList: List[String] = cards.grouped(2).toList
 }
 
 object StCards {
 
   def apply(asPairs: Set[(Rank, Suit)]): StCards =
     new StCards(asPairs.map(p => Cards.fromRank(p._1) + p._2).mkString(""))
+
+  def apply(cards: Cards): StCards = cards match {
+    case stCards: StCards => stCards
+    case _                => StCards(cards.toString)
+  }
 }
--- src/main/scala/net/cheltsov/poker/string/StHand.scala
+++ src/main/scala/net/cheltsov/poker/string/StHand.scala
@@ -1,8 +1,22 @@
 package net.cheltsov.poker.string
 
-import net.cheltsov.poker.Hand
+import net.cheltsov.poker.{Cards, Hand}
 
-class StHand(override val cards: String) extends StCards(cards) with Hand
+class StHand(override val cards: String) extends StCards(cards) with Hand {
+
+  override protected def findSameRankCards(amount: Order)(cards: Cards): Option[Cards] =
+    StCards(cards).asList
+      .groupMapReduce(_.head)(identity)(_ + _)
+      .filter(_._2.length == amount * 2)
+      .values
+      .map(StCards(_))
+      .headOption
+
+  override protected def findFlush(cards: Cards): Option[Cards] = {
+    val stCards = StCards(cards)
+    Option.when(stCards.asList.map(_.last).distinct.size == 1)(stCards)
+  }
+}
 
 object StHand {
   def apply(stCards: StCards): StHand = new StHand(stCards.cards)
